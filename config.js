fc4383be2c8ecc6f4db2acc57dbf2870eb8f8dbc
Glen Maddern <glenmaddern@gmail.com>
2015-01-09 14:56:46 +1100

    actually doing some peak detection now

--- config.js
+++ config.js
@@ -1,7 +1,14 @@
 System.config({
   "paths": {
     "*": "*.js",
-    "app/*": "lib/*.js"
+    "app/*": "lib/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "fkit": "npm:fkit@0.16.2"
   }
 });
 
